441b2a992afa781b1cb6b90a2bbfa0e886db82c1
tapan kumar swain <tapank.swain@hyscaler.com>
2026-01-08 15:23:12 +0530

    feat: fix issue where track continued playing after app reload

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { ThemeProvider } from './src/core/theme/ThemeContext';
-import { PlayerProvider } from './src/contexts/PlayerContext';
-import { RootNavigator } from './src/navigation/RootNavigator';
+import React, {useEffect} from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {ThemeProvider} from './src/core/theme/ThemeContext';
+import {PlayerProvider} from './src/contexts/PlayerContext';
+import {RootNavigator} from './src/navigation/RootNavigator';
+import {cleanupPlayer} from './src/services/PlayerService';
 
 const App = () => {
+  useEffect(() => {
+    return () => {
+      cleanupPlayer();
+    };
+  }, []);
+
   return (
     <SafeAreaProvider>
       <PlayerProvider>
