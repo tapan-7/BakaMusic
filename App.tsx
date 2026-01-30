c1312f40c1b319bae3beb04369285dcb409cb0f5
tapan kumar swain <tapank.swain@hyscaler.com>
2026-01-30 16:13:48 +0530

    feat: integrate redux for storing single or currently playing track

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
-import React, {useEffect} from 'react';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {ThemeProvider} from './src/core/theme/ThemeContext';
-import {PlayerProvider} from './src/contexts/PlayerContext';
-import {RootNavigator} from './src/navigation/RootNavigator';
-import {cleanupPlayer} from './src/services/PlayerService';
+import React, { useEffect } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider } from './src/core/theme/ThemeContext';
+import { PlayerProvider } from './src/contexts/PlayerContext';
+import { RootNavigator } from './src/navigation/RootNavigator';
+import { cleanupPlayer } from './src/services/PlayerService';
+
+import { Provider } from 'react-redux';
+import { store } from './src/store';
 
 const App = () => {
   useEffect(() => {
@@ -13,13 +16,15 @@ const App = () => {
   }, []);
 
   return (
-    <SafeAreaProvider>
-      <PlayerProvider>
-        <ThemeProvider>
-          <RootNavigator />
-        </ThemeProvider>
-      </PlayerProvider>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <PlayerProvider>
+          <ThemeProvider>
+            <RootNavigator />
+          </ThemeProvider>
+        </PlayerProvider>
+      </SafeAreaProvider>
+    </Provider>
   );
 };
 
